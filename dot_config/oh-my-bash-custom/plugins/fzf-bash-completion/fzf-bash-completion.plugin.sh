30c7ea9bdda28a0e2854a5db9f8c08b5adda5a9b
Pascal Kontschan <pascal.kontschan@sva.de>
2025-09-30 20:20:33 +0200

    fzf

--- dot_config/oh-my-bash-custom/plugins/fzf-bash-completion/fzf-bash-completion.plugin.sh
+++ dot_config/oh-my-bash-custom/plugins/fzf-bash-completion/fzf-bash-completion.plugin.sh
@@ -1,2 +1,11 @@
 source $OSH-custom/plugins/fzf-bash-completion/fzf-bash-completion.sh
+source $OSH-custom/plugins/fzf-bash-completion/fzf-theme.sh
+
 bind -x '"\t": fzf_bash_completion'
+
+#export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+#--bind f1:execute(less -f {})
+#'
+
+export FZF_COMPLETION_AUTO_COMMON_PREFIX=true
+export FZF_COMPLETION_AUTO_COMMON_PREFIX_PART=true
---
+++ dot_config/oh-my-bash-custom/plugins/fzf-bash-completion/fzf-theme.sh
@@ -0,0 +1,8 @@
+export FZF_DEFAULT_OPTS='
+  --color=fg:#ebdbb2,fg+:#fbf1c7,bg:#32302f,bg+:#504945
+  --color=hl:#458588,hl+:#83a598,info:#a89984,marker:#b8bb26
+  --color=prompt:#83a598,spinner:#b16286,pointer:#d3869b,header:#8ec07c
+  --color=border:#d5c4a1,label:#928374,query:#ebdbb2
+  --border="rounded" --border-label="" --preview-window="border-rounded" --padding="0,1"
+  --prompt=" " --marker=">" --pointer="◆" --separator="─"
+  --scrollbar="│" --info="right" --bind "f1:execute(nvim {})"'
