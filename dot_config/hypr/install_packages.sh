b44eb8724580944916fbf9942af20442f324d0f6
Pascal Kontschan <pascal.kontschan@sva.de>
2025-10-13 10:45:28 +0200

    workspace config

--- dot_config/hypr/install_packages.sh
+++ dot_config/hypr/install_packages.sh
@@ -5,14 +5,17 @@ packages=(
   1password-cli
   fzf
   htop
-  lsd       # better ls
-  hyprland  # the compositor
-  hyprpaper # wallpaper
-  hypridle  # idle detection and tooling
-  hyprlock  # lock screen
-  hyprshot  # screenshot taker
-  satty     # screenshot editor
-  cliphist  # clipboard history
+  lsd             # better ls
+  hyprland        # the compositor
+  hyprpaper       # wallpaper
+  hypridle        # idle detection and tooling
+  hyprlock        # lock screen
+  hyprshot        # screenshot taker
+  hyprpolkitagent # popup when software requests elevated permissions
+  satty           # screenshot editor
+  cliphist        # clipboard history
+  qt5-qtwayland
+  qt5-qtwayland
 )
 
 sudo dnf copr enable solopasha/hyprland
