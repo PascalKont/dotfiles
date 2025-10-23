ea4ce39d0b49ccfaf8beea0d36c33c4bdcf0ff94
Pascal Kontschan <pascal.kontschan@sva.de>
2025-10-23 11:55:08 +0200

    update

--- dot_config/nvim/lua/plugins/explorer.lua
+++ dot_config/nvim/lua/plugins/explorer.lua
@@ -14,6 +14,7 @@ return {
             -- your explorer picker configuration comes here
             -- or leave it empty to use the default settings
             autoclose = "true",
+            hidden = "true",
             layout = { preset = "sidebar", preview = true },
           },
         },
