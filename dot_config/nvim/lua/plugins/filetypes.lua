a9ce90763d7ded81ab8c29924dc8c13ab7c0f81e
Pascal Kontschan <pascal@kontschan.de>
2025-11-10 13:24:21 +0100

    filetypes

--- dot_config/nvim/lua/plugins/filetypes.lua
+++ dot_config/nvim/lua/plugins/filetypes.lua
@@ -1,11 +1,14 @@
 return {
+  {
+    "prettier/vim-prettier",
+  },
   vim.filetype.add({
     pattern = {
       [".*%.toml%.tmpl"] = "toml",
       [".*%.bash%.tmpl"] = "bash",
       [".*%.sh%.tmpl"] = "sh",
-      ["%.ansible%-lint"] = "yaml",
-      -- [".*%.yml"] = "yaml.ansible",
+      ["%.ansible%-lint"] = "yaml.ansible",
+      [".*%.yml"] = "yaml.ansible",
     },
   }),
 }
