b413ec217eba9be6dc4032deae227026ced97379
Pascal Kontschan <pascal.kontschan@sva.de>
2025-12-17 11:53:07 +0100

    added filetypes

--- dot_config/nvim/lua/plugins/filetypes.lua
+++ dot_config/nvim/lua/plugins/filetypes.lua
@@ -5,8 +5,9 @@ return {
   vim.filetype.add({
     pattern = {
       [".*%.toml%.tmpl"] = "toml",
-      [".*%.bash%.tmpl"] = "bash",
+      [".*%.bash%.tmpl"] = "sh",
       [".*%.sh%.tmpl"] = "sh",
+      [".*%.rc"] = "sh",
       ["%.ansible%-lint"] = "yaml.ansible",
       [".*%.gitlab%-ci.yml"] = "yaml.gitlab",
       [".*%.yml"] = "yaml.ansible",
