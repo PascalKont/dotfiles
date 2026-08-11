a743ea1dfaaa13f5ccbf705f1e6c0168dee1e57c
Pascal Kontschan <pascal.kontschan@sva.de>
2026-08-11 19:10:43 +0200

    detect hyprland

--- .ignored/detect_hyprland.sh
+++ .ignored/detect_hyprland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if /usr/bin/hyprland --version 1>/dev/null 2>&1; then
-  /usr/bin/echo -n "true"
-else /usr/bin/echo -n "false"; fi
+if which hyprland 1>/dev/null 2>&1; then
+  echo -n "true"
+else echo -n "false"; fi
