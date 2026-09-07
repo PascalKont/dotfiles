477f0a5293e64d98053c842fb013e5ee8620c3e6
Pascal Kontschan <pascal.kontschan@sva.de>
2026-09-07 11:22:49 +0200

    disable hyprland decorations

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -120,14 +120,14 @@ hl.config({
 		inactive_opacity = 1.0,
 
 		shadow = {
-			enabled = true,
+			enabled = false,
 			range = 4,
 			render_power = 3,
 			color = 0x1a1a1aee,
 		},
 
 		blur = {
-			enabled = true,
+			enabled = false,
 			size = 3,
 			passes = 1,
 			vibrancy = 0.1696,
